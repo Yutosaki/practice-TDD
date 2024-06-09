443c748ee0a6780d6d61e132b83ed356bebe7bd9
Yuto <sasayu8190@gmail.com>
2024-06-09 13:26:35 +0900

    add: hello

---
+++ hello/hello_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	asserCorrectMessage := func(t testing.TB, got, want string){
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello("Chris", "")
+		want := "Hello, Chris"
+		asserCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := hello("", "")
+		want := "Hello, world"
+		asserCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T){
+		got := hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		asserCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T){
+		got := hello("josh", "French")
+		want := "Bonjur, josh"
+		asserCorrectMessage(t, got, want)
+	})
+}
